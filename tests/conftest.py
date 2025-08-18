a33de54b3338e45294c967b49182401933ee3bf0
Alena Kit <anashkina.kit@yandex.ru>
2025-08-18 20:11:17 +0300

    поправила conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,7 @@
 import pytest
 from unittest.mock import MagicMock
-from praktikum.burger import Burger
 from praktikum.bun import Bun
 from praktikum.ingredient import Ingredient
-from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 
 
 @pytest.fixture
@@ -29,22 +27,7 @@ def mock_ingredient(request):
     mock.get_price.return_value = price
     return mock
 
-# ниже уберу
 
 
 
-@pytest.fixture
-def burger():
-    return Burger()
 
-@pytest.fixture
-def bun():
-    return Bun("Test Bun", 100)
-
-@pytest.fixture
-def sauce():
-    return Ingredient(INGREDIENT_TYPE_SAUCE, "Sauce", 50)
-
-@pytest.fixture
-def filling():
-    return Ingredient(INGREDIENT_TYPE_FILLING, "Filling", 100)
